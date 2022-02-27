ddd89258898dd4de56859848f3df62b759d9b1a2
Vinny <vincentmichaelsmith@gmail.com>
2022-02-26 18:03:19 -0700

    Question 1 Done

--- client/components/question1/_question_1.jsx
+++ client/components/question1/_question_1.jsx
@@ -1,3 +1,8 @@
+import { Counter } from "./counter";
 export const Question1 = () => {
-  return <div>Render your counters here!</div>;
+  return <div>Render your counters here!
+    <Counter min={-5} max ={5}></Counter>
+    <Counter min={-15} max ={-5}></Counter>
+    <Counter min={5} max ={15}></Counter>
+  </div>;
 };
---
+++ client/components/question1/counter.jsx
@@ -0,0 +1,27 @@
+import { Button } from '../common/button';
+import { useState, useEffect } from 'react';
+// stole the button from the starter app
+export const Counter = ({min,max}) => {
+    const [count, setCount] = useState(0);
+    useEffect(()=>{
+
+    });
+    const minus = () => {
+        if (count == min) return;
+        else {setCount(count-1);}
+    };
+    const plus = () => {
+        if (count == max) return;
+        else {setCount(count+1);}
+    };
+    if (min > 0 && count === 0) setCount(min);
+    if (max < 0 && count === 0) setCount(max);
+    return (
+        <div className="p-4">
+            <div className="content-center">{count}</div>
+            <Button onClick={minus}>-</Button>
+            <Button onClick={plus}>+</Button>
+
+        </div>
+    );
+};
